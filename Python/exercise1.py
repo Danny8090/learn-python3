5d92238e1d58a5e5f9ec48d7dd0c060edf77f26e
danny ding <danny8090@foxmail.com>
2018-04-19 21:32:25 +0800

    20180419change

---
+++ Python/exercise1.py
@@ -0,0 +1,13 @@
+# 转义字符
+print('\\\t\\')
+print('\\\n\\')
+print('I\'m ok,')
+print('I\'m learing\nPython')
+print(r'\\\t\\')
+print('''line1
+...line2
+...line3''')
+
+print('''line1
+... line2
+... line3''')
